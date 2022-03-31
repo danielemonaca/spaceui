21244ed3c55a6a11fda5ef7a18289524099d3d76
danielemonaca <daniele.monaca1@gmail.com>
2022-03-31 14:44:47 +0200

    feat: implements styled component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,4 @@
 import React, { FC } from 'react';
-import cx from 'classnames';
-import './styles.css';
 
 export interface IButtonProps {
   onClick: () => void;
@@ -9,8 +7,9 @@ export interface IButtonProps {
   text?: string;
 }
 
+
 export const Button: FC<IButtonProps> = ({ onClick, disabled, className, text }) => (
-  <button type="button" className={cx('button', className)} onClick={onClick} disabled={disabled}>
+  <button type="button" className={className} onClick={onClick} disabled={disabled}>
     {text}
   </button>
 );
